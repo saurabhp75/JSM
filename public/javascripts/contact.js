56c12ae453e16a846b8a88302db18f6714c08668
Dariusz Stefanski <dstefanski@atlassian.com>
2016-08-12 12:48:57 +1000

    disbaling form while issuing the request

--- public/javascripts/contact.js
+++ public/javascripts/contact.js
@@ -1,18 +1,23 @@
 $(function () {
     $("#contact").click(function () {
         var formFields = {};
-        $("form input").each(function() {
+        var formInputs = $("form input");
+        formInputs.each(function() {
             formFields[this.name] = $(this).val();
         });
+
+        formInputs.prop("disabled", true);
+        $("button").prop("disabled", true);
+        $("button").text("Please wait...");
 
         $.ajax("/contact", {
             type: "POST",
             contentType: "application/json",
             data: JSON.stringify(formFields)
         }).done(function (issueKey) {
-            $(".aui-item").html("<p>Thank you. We will get back to you ASAP. Your issue key is " + issueKey + "</p>")
+            $(".content").html("<p>Thank you. We will get back to you ASAP. Your issue key is " + issueKey + "</p>")
         }).fail(function (jqXhr) {
-            $(".aui-item").html("Failed creating request: " + jqXhr.responseText + " (" + jqXhr.status + " - " + jqXhr.statusText + ")");
+            $(".content").html("Failed creating request: " + jqXhr.responseText + " (" + jqXhr.status + " - " + jqXhr.statusText + ")");
         });
 
         return false;
