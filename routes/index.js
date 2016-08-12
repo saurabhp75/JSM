14f049267706b05c499639bc8879acee35c68761
Dariusz Stefanski <dstefanski@atlassian.com>
2016-08-12 11:42:30 +1000

    More refactoring: * request defaults * get rid of var * missing semicolons * arrow funtion * more logs in error handler * fix parameteres issues

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,21 @@ const config = require("../config.json");
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  request.get({
-    url: jsdRestApiUrl(`/servicedesk/${config.serviceDesk.id}/requesttype/${config.serviceDesk.requestTypeId}/field`),
-    auth: auth()
-  }, (err, httpResponse, body) => {
+const jsdRequest = request.defaults({
+  baseUrl: config.instance.url + "/rest/servicedeskapi",
+  auth: {
+    'user': config.instance.username,
+    'pass': config.instance.password
+  },
+  json: true
+});
+
+router.get('/', (req, res) => {
+  jsdRequest.get(`/servicedesk/${config.serviceDesk.id}/requesttype/${config.serviceDesk.requestTypeId}/field`,
+  (err, httpResponse, body) => {
     if (httpResponse.statusCode === 200) {
-      const requestTypeFields = JSON.parse(body).requestTypeFields;
-      var fields = extractStringFields(requestTypeFields);
+      const requestTypeFields = body.requestTypeFields;
+      const fields = extractStringFields(requestTypeFields);
       res.render('contact', {fields: fields});
     } else {
       writeError(res, httpResponse, body);
@@ -35,16 +42,14 @@ router.post("/contact", (req, res) => {
   const formFields = req.body;
 
   const customerPayload = getCustomerPayload(formFields);
-  request.post({
-    url: jsdRestApiUrl("/customer"),
+  jsdRequest.post({
+    url: "/customer",
     body: customerPayload,
-    json: true,
     headers: {
       "X-ExperimentalApi": true
-    },
-    auth: auth()
+    }
   }, (err, httpResponse, body) => {
-    var username;
+    let username;
     if (httpResponse.statusCode === 201) {
       username = body.name;
     } else if (userExists(httpResponse, body)) {
@@ -57,14 +62,12 @@ router.post("/contact", (req, res) => {
     createRequest(res, username, formFields);
   });
 
-  function createRequest(username, formFields) {
+  function createRequest(res, username, formFields) {
     const requestPayload = getRequestPayload(username, formFields);
-    request.post({
-      url: jsdRestApiUrl("/request"),
-      body: requestPayload,
-      json: true,
-      auth: auth()
-    }, (res, err, httpResponse, body) => {
+    jsdRequest.post({
+      url: "/request",
+      body: requestPayload
+    }, (err, httpResponse, body) => {
       if (httpResponse.statusCode < 200 || httpResponse.statusCode >= 300) {
         writeError(res, httpResponse, body);
         return;
@@ -101,8 +104,8 @@ router.post("/contact", (req, res) => {
   }
 
   function fieldsWithoutEmail(fields){
-    const clonedFields = clone(fields)
-    delete clonedFields.email
+    const clonedFields = clone(fields);
+    delete clonedFields.email;
     return clonedFields;
   }
 });
@@ -112,21 +115,10 @@ function clone(obj) {
 }
 
 function writeError(res, httpResponse, body) {
-  console.log("Operation failed");
+  console.error(`Operation failed with ${httpResponse.statusMessage} (${httpResponse.statusCode})`);
   res.statusCode = httpResponse.statusCode;
   res.statusMessage = httpResponse.statusMessage;
   res.end(body.errorMessage);
-}
-
-function auth() {
-  return {
-    'user': config.instance.username,
-    'pass': config.instance.password
-  }
-}
-
-function jsdRestApiUrl(suffix) {
-  return config.instance.url + "/rest/servicedeskapi" + suffix;
 }
 
 module.exports = router;
